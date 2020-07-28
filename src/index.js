45764ef63a125df3c8c33a882f8d902ca0d8a40d
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-07-28 23:52:01 +0500

    Install webpack and prepare it for work with Three.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import * as THREE from 'three';
+import './styles/style.css';
+
+
 function sceneOnLoad() {
     let scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -9,6 +13,7 @@ function sceneOnLoad() {
     let geometry = new THREE.BoxGeometry();
     let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     let cube = new THREE.Mesh(geometry,material);
+    
     scene.add(cube);
 
     camera.position.z = 5;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+}
