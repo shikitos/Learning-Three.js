41f0fe42667d5a3261f83fb3005f8b957426acbc
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-07-29 00:52:31 +0500

    Configuring a webpack. Add babel and minimizer of all files

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,74 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDevMode = process.env.NODE_ENV === 'development';
+const isProdMode = !isDevMode;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+    if (isProdMode) {
+        config.minimizer = [
+            new OptimizeCssAssetsPlugin(),
+            new TerserWebpackPlugin(),
+        ]
+    }
+    return config
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
-    entry: './index.js',
+    entry: [
+        '@babel/polyfill',
+        './index.js'
+    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    optimization: optimization(),
     plugins: [
         new HTMLWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProdMode,
+            }
         }),
+        new MiniCssExtractPlugin(),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hrm: isDevMode,
+                            reloadAll: true
+                        }
+                    },
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
             }
         ]
     }
