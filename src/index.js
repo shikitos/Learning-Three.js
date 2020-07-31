a78307dc587985cfc59fe01637e7585742b0632a
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-07-31 23:54:35 +0500

    Add first animation

--- src/index.js
+++ src/index.js
@@ -5,30 +5,67 @@ import './styles/style.css';
 
 
 function sceneOnLoad() {
+    //create scene
     let scene = new THREE.Scene();
+    //create camera
     let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-    let renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    let geometry = new THREE.BoxGeometry();
-    let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    let cube = new THREE.Mesh(geometry,material);
-    
-    scene.add(cube);
-
+    let renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
     camera.position.z = 5;
+    //add color for renderer
+    renderer.setClearColor('#e5e5e5');
+    //add size (which will be fullscreen)
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    //add renderer at the page
+    document.body.appendChild(renderer.domElement);
+    //every time when the user resize the window...
+    window.addEventListener('resize', () => {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    });
 
-    function animate() {
-        requestAnimationFrame(animate);
+    //create new object - sphere
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    //add material for the sphere
+    let material = new THREE.MeshLambertMaterial({color: 0xFFCC00});
+    //combine geometry + material = figure
+    let mesh = new THREE.Mesh(geometry, material);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+    mesh.position.set(2, 2, -2);
 
+
+    //add it at the scene
+    scene.add(mesh);
+    /*
+    * As we can see - our figure is black
+    * cuz we need light
+    * and we create it
+     */
+    let light = new THREE.PointLight(0xFFFFFF, 1, 500);
+    //put light into (10, 0, 25) position
+    light.position.set(10, 0, 25);
+    //add light at the scene
+    scene.add(light);
+
+    //render the scene
+    let render = function() {
+        //loop which as page resize or refresh — rerender the scene
+        requestAnimationFrame(render);
+        mesh.rotation.x += 0.01;
+        mesh.rotation.y += 0.01;
         renderer.render(scene, camera);
     }
-    animate();
+    render();
+
+    this.tl = new TimelineMax().delay(0.3);
+    this.tl.to(this.mesh.scale, 1, {x: 2, ease: Expo.easeOut});
+    this.tl.to(this.mesh.scale, 0.5, {x: 0.5, ease: Expo.easeOut});
+    this.tl.to(this.mesh.position, 0.5, {x: 2, ease: Expo.easeOut});
+    this.tl.to(this.mesh.rotation, 0.5, {y: Math.PI*0.5, ease: Expo.easeOut}, '=-1.5');
+
+
 }
 
 sceneOnLoad();
